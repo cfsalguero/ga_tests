cd818c1f2ae6d0e57c1bab61b6f7ad953f4212c9
Carlos Salguero <cfsalguero@gmail.com>
2023-10-24 15:23:03 -0300

    Added mod

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 	fmt.Println("vim-go")
 
-	a := 0
+	v, err := fna(1)
+	if err != nil {
+		os.Exit(1)
+	}
+
+	fmt.Println(v)
+}
+
+func fna(i int) (int, error) {
+	if i < 0 {
+		return 0, fmt.Errorf("negative numbers are not allowed")
+	}
+
+	return i + 1, nil
 }
